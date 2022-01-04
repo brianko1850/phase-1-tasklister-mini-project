632f313f033114c020cc6522088097c3f0637a0a
brianko <kobj11@gmail.com>
2022-01-04 17:12:31 -0500

    tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+let form = document.querySelector('form')
+form.addEventListener('submit', (e) => {
+     e.preventDefault()
+     handleToDo(document.getElementById("new-task-description").value)
+     form.reset()
+  
+    
+   })
+ });
+
+  function handleToDo(todo) {
+    let btn = document.createElement('button')
+    let p = document.createElement('p')
+    p.textContent = `${todo} `
+    btn.textContent = "x"
+    btn.addEventListener('click', handleDelete)
+    document.querySelector('#tasks').appendChild(p)
+    p.appendChild(btn)
+  }
+
+  function handleDelete(e) {
+    e.target.parentNode.remove()
+  }
